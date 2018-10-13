a341aafe3c066775f6a322f2accd2dbb227c2021
pol9111 <biscuit36@163.com>
2018-10-13 22:30:31 +0800

    version 1.2

--- saver.py
+++ saver.py
@@ -3,7 +3,6 @@ from config import LAGOU_ITEMS, TABLE
 
 
 async def get_data(loop):
-    """暂存redis"""
     print('开始存入mongo..')
     redis = await aioredis.create_redis_pool(
         'redis://localhost', loop=loop)
